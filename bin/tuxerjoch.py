36aec2e56e9d8cb2f3a310e73ee2932f41b14031
Olaf Radicke <briefkasten@olaf-radicke.de>
2015-07-03 19:10:46 +0200

    Refactoring

--- bin/tuxerjoch.py
+++ bin/tuxerjoch.py
@@ -7,101 +7,134 @@ import controls.auth
 import controls.home
 import couch_backend.rest
 
-app = bottle.Bottle()
+class Tuxerjoch:
+
+    def __init__(self):
+        '''Constructor do some preparations for app environment'''
+        self.app = bottle.Bottle()
+        self.read_config()
+        self.config_logger()
+        self.config_database_connect()
+        self.check_database_status()
+        self.init_controller()
+        self.set_routs()
+
+    def read_config(self):
+        '''Reade config file '''
+        with open("tuxerjoch.conf") as json_file:
+            self.config_data = json.load(json_file)
+
+    def config_logger(self):
+        '''Prepare logger'''
+        if self.config_data["log_level"] == "DEBUG":
+            logging.basicConfig(
+                level=logging.DEBUG,
+                format='%(asctime)s %(name)-2s %(levelname)-2s %(message)s',
+                datefmt='%m-%d %H:%M',
+                filename=self.config_data["log_file"],
+                filemode='w'
+            )
+        if self.config_data["log_level"] == "INFO":
+            logging.basicConfig(
+                level=logging.INFO,
+                format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
+                datefmt='%m-%d %H:%M',
+                filename=self.config_data["log_file"],
+                filemode='w'
+            )
+        if self.config_data["log_level"] == "ERROR":
+            logging.basicConfig(
+                level=logging.ERROR,
+                format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
+                datefmt='%m-%d %H:%M',
+                filename=self.config_data["log_file"],
+                filemode='w'
+            )
+
+    def config_database_connect(self):
+        '''Prepare database connect'''
+        self.restWrapper = couch_backend.rest.RestWrapper()
+        self.restWrapper.setHost( self.config_data["couch_host"] )
+        self.restWrapper.setPort( self.config_data["couch_port"] )
+        self.restWrapper.setPassword( self.config_data["couch_passwd"] )
+        self.restWrapper.setUser( self.config_data["couch_user"] )
+        self.restWrapper.setDB( self.config_data["couch_db"] )
+
+    def check_database_status(self):
+        '''check database'''
+        response = self.restWrapper.getAllDBs()
+        logging.debug( "Founded data bases: ")
+        logging.debug( response.text )
+        if self.config_data["couch_db"] in json.loads(response.text):
+            logging.debug("I found database " + self.config_data["couch_db"])
+        else:
+            logging.info( "I can not found database " + self.config_data["couch_db"] + " and create now!")
+            response = self.restWrapper.createDB( self.config_data["couch_db"] )
+            logging.info( response.text )
+
+    def check_password_protection(self):
+        '''check password protection'''
+        response = self.restWrapper.getDocValue( "auth_key" )
+        print( response.text )
+        auth_key_data = json.loads(response.text, 'utf8')
+        if "error" in auth_key_data:
+            if auth_key_data["error"] == "not_found":
+                logging.info( "Can not found document auth_key and create with default password" )
+                json_code = '{ \n'
+                json_code += '    "document_type": "app_config", \n'
+                json_code += '    "passwd_hash": "613367845fd07938881688f6c7e222497d778db3c3d7ff85c764498347d495c9", \n'
+                json_code += '    "salt": "tuxerjoch", \n'
+                # signature key required for signed cookies
+                json_code += '    "cookie_secret_key": "tuxerjoch", \n'
+                # cookie live time maximum age in seconds
+                json_code += '    "cookie_live_time": 7200 \n'
+                json_code += '}'
+
+                response = self.restWrapper.insertNamedDoc( "auth_key", json_code )
+                logging.info( response.text )
+            else:
+                logging.error( "Unknown error: " )
+                logging.error( auth_key_data["error"] + ": " + auth_key_data["reason"] )
 
 
-# Reade config file
-with open("tuxerjoch.conf") as json_file:
-    config_data = json.load(json_file)
 
-# prepare logger
-if config_data["log_level"] == "DEBUG":
-    logging.basicConfig(
-        level=logging.DEBUG,
-        format='%(asctime)s %(name)-2s %(levelname)-2s %(message)s',
-        datefmt='%m-%d %H:%M',
-        filename=config_data["log_file"],
-        filemode='w'
-    )
-if config_data["log_level"] == "INFO":
-    logging.basicConfig(
-        level=logging.INFO,
-        format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
-        datefmt='%m-%d %H:%M',
-        filename=config_data["log_file"],
-        filemode='w'
-    )
-if config_data["log_level"] == "ERROR":
-    logging.basicConfig(
-        level=logging.ERROR,
-        format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
-        datefmt='%m-%d %H:%M',
-        filename=config_data["log_file"],
-        filemode='w'
-    )
+    def static_file_get( self, filename ):
+        '''Get back static content'''
+        return bottle.static_file(filename, root='/static/')
 
-# prepare database connect
-restWrapper = couch_backend.rest.RestWrapper()
-restWrapper.setHost( config_data["couch_host"] )
-restWrapper.setPort( config_data["couch_port"] )
-restWrapper.setPassword( config_data["couch_passwd"] )
-restWrapper.setUser( config_data["couch_user"] )
-restWrapper.setDB( config_data["couch_db"] )
+    def init_controller(self):
+        # init controller
+        self.home_page          = controls.home.Home( self.restWrapper )
+        self.controllNewArticle = controls.article.NewArticle( self.restWrapper )
+        self.controllAuth       = controls.auth.Auth( self.restWrapper )
 
-# check database
-response = restWrapper.getAllDBs()
-logging.debug( "Founded data bases: ")
-logging.debug( response.text )
-if config_data["couch_db"] in json.loads(response.text):
-    logging.debug("I found database " + config_data["couch_db"])
-else:
-    logging.info( "I can not found database " + config_data["couch_db"] + " and create now!")
-    response = restWrapper.createDB( config_data["couch_db"] )
-    logging.info( response.text )
+    def set_routs(self):
+        '''set routs'''
 
+        self.app.route('/static/<filename>', ['GET'], self.static_file_get)
+        self.app.route('/', ['GET'],
+                       self.home_page.start_get)
+        self.app.route('/login', ['GET'],
+                       self.controllAuth.login_get)
+        self.app.route('/login', ['POST'],
+                       self.controllAuth.login_post)
+        self.app.route('/logout', ['GET'],
+                       self.controllAuth.logout_get)
+        self.app.route('/new_article', ['GET'],
+                       self.controllNewArticle.new_get)
+        self.app.route('/new_article', ['POST'],
+                       self.controllNewArticle.new_post)
+        self.app.route('/view_article/<name>', ['GET'],
+                       self.controllNewArticle.view_article_get)
 
-# check password protection
-response = restWrapper.getDocValue( "auth_key" )
-print( response.text )
-auth_key_data = json.loads(response.text, 'utf8')
-if "error" in auth_key_data:
-    if auth_key_data["error"] == "not_found":
-        logging.info( "Can not found document auth_key and create with default password" )
-        json_code = '{ \n'
-        json_code += '    "document_type": "app_config", \n'
-        json_code += '    "passwd_hash": "613367845fd07938881688f6c7e222497d778db3c3d7ff85c764498347d495c9", \n'
-        json_code += '    "salt": "tuxerjoch", \n'
-        # signature key required for signed cookies
-        json_code += '    "cookie_secret_key": "tuxerjoch", \n'
-        # cookie live time maximum age in seconds
-        json_code += '    "cookie_live_time": 7200 \n'
-        json_code += '}'
+    def run(self):
+        '''Start listening'''
+        bottle.run(
+            self.app,
+            host=self.config_data["webservice_host"],
+            port=self.config_data["webservice_port"],
+            server='cherrypy'
+            )
 
-        response = restWrapper.insertNamedDoc( "auth_key", json_code )
-        logging.info( response.text )
-    else:
-        logging.error( "Unknown error: " )
-        logging.error( auth_key_data["error"] + ": " + auth_key_data["reason"] )
-
-
-# init controller
-home_page          = controls.home.Home( restWrapper )
-controllNewArticle = controls.article.NewArticle( restWrapper )
-controllAuth       = controls.auth.Auth( restWrapper )
-
-# set routs
-app.route('/', ['GET'], home_page.start_get)
-app.route('/login', ['GET'], controllAuth.login_get)
-app.route('/login', ['POST'], controllAuth.login_post)
-app.route('/logout', ['GET'], controllAuth.logout_get)
-app.route('/new_article', ['GET'], controllNewArticle.new_get)
-app.route('/new_article', ['POST'], controllNewArticle.new_post)
-app.route('/view_article/<name>', ['GET'], controllNewArticle.view_article_get)
-
-
-bottle.run(
-    app,
-    host=config_data["webservice_host"],
-    port=config_data["webservice_port"],
-    server='cherrypy'
-    )
+tuxerjoch = Tuxerjoch()
+tuxerjoch.run()
